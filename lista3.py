7c780fc4496a85a8559c18afaa8dedc8681d2bad
Daniellima-DEV532 <dan.lima.alencar@gmail.com>
2026-06-22 11:10:29 -0400

    Add files via upload

---
+++ lista3.py
@@ -0,0 +1,21 @@
+1
+#valor1=float(input('Digite o primeiro valor:'))
+#valor2=float(input('Digite o segundo valor:'))
+#valor3=float(input('Digite o terceiro valor'))
+ 
+#soma=valor1+valor2+valor3
+#media=soma/3
+#print('Soma e igual a:',soma)
+#print('Meida e igual a:',media)
+2
+#distancia = float(input("Digite a distância: "))
+#tempo = float(input("Digite o tempo: "))
+
+#velocidade = distancia / tempo
+
+#print("Velocidade média:", velocidade)
+3
+distancia=float(input('Digite a distancia precorrida:'))
+consumo=float(input('digite o valor do consumo:'))
+litros=distancia/consumo
+print('Litros necessarios:',litros)
